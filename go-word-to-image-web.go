db34c6fa823a341e79d621c4a43c5351ee9e5a51
marat <marat.art@gmail.com>
2015-08-19 13:07:51 +0300

    Test package

--- go-word-to-image-web.go
+++ go-word-to-image-web.go
@@ -1,6 +1,21 @@
 /*
 Go-word-to-image-web is a web interface to go-word-to-image.
 */
+package main
+
 import (
-  wi "github.com/maratart/go-word-to-image"
+	"fmt"
+	"log"
+
+	wi "github.com/maratart/go-word-to-image/wordtoimage"
 )
+
+func main() {
+	word := "test"
+	imageURL, err := wi.GetImageURL(word)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(imageURL)
+}
