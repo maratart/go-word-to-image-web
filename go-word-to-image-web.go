c7d8f8a786393f1d16f0e8bee3baf66196704a8c
marat <marat.art@gmail.com>
2015-08-19 15:01:00 +0300

    added simple web interface

--- go-word-to-image-web.go
+++ go-word-to-image-web.go
@@ -6,16 +6,46 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	wi "github.com/maratart/go-word-to-image/wordtoimage"
 )
 
-func main() {
-	word := "test"
-	imageURL, err := wi.GetImageURL(word)
-	if err != nil {
-		log.Println(err)
-		return
+const form = `
+<div style="text-align: center; margin-top: 50px;">
+<form action="post">
+<input type="text" name="word" size="40" value="%s" autofocus="autofocus" >
+<input type="submit" value="Translate to image">
+</form>
+</div>
+`
+const picture = `
+<div style="text-align: center">
+<img src='%s' style='max-width: 600px; max-height: 600px;' />
+</div>
+`
+
+func handlerShowImage(w http.ResponseWriter, r *http.Request) {
+	// set content-type for the answer page
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// get word
+	word := r.FormValue("word")
+	// show form
+	fmt.Fprintf(w, form, word)
+	if len(word) > 0 {
+		// translate word to image
+		imageURL, err := wi.GetImageURL(word)
+		// show image
+		if err != nil {
+			log.Println(err)
+			fmt.Fprintf(w, "Error: %s", err)
+		} else {
+			fmt.Fprintf(w, picture, imageURL)
+		}
 	}
-	fmt.Println(imageURL)
+}
+
+func main() {
+	http.HandleFunc("/", handlerShowImage)
+	http.ListenAndServe(":8001", nil)
 }
